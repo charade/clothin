ebde27529cd28a91c31bd3ec50dc7187d6c965c0
Charles.Ek <macbook@MacBook.local>
2021-11-15 12:50:12 +0100

    :zap:

--- src/assets/styles/Navbar.styles.ts
+++ src/assets/styles/Navbar.styles.ts
@@ -4,16 +4,14 @@ export const useNavbarStyle = makeStyles(theme => ({
     root : {
         zIndex : 9,
         height : '60px',
-        background : 'rgb(14, 25,40)',
+       
         overflow : 'hidden',
         width :'100%',
-        boxShadow : 'none',
-        borderBottom :'2px solid rgba(255, 255, 255, .7)',
         display : 'flex',
         justifyContent : 'flex-end',
-        backdropFilter : 'blur(20px)'
     },
     toolbar : {
+        background : 'rgb(14, 25,40)',
         display : 'flex',
         alignItems: 'center',
         justifyContent :'space-around',
